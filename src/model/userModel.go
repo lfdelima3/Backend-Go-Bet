91b50c07033de554abc74e6cd084127598e13898
Luis Fernando <lfdelimaa@gmail.com>
2025-05-28 21:08:49 -0300

    kkk

--- src/model/userModel.go
+++ src/model/userModel.go
@@ -8,9 +8,11 @@ import (
 
 type User struct {
 	gorm.Model
-	Username  string    `gorm:"type:varchar(100) json:"name" validate:"required"`
-	Email     string    `gorm:"type:varchar(100);uniqueIndex;not null" json:"email" validate:"required,email"`
-	Password  string    `gorm:"type:varchar(100);not null" json:"-" validate:"required,min=6"`
+	Username string `gorm:"type:varchar(100) json:"name" validate:"required"`
+	Email    string `gorm:"type:varchar(100);uniqueIndex;not null" json:"email" validate:"required,email"`
+	Password string `gorm:"type:varchar(100);not null" json:"-" validate:"required,min=6"`
+	//Data_nasc
+	//Time_Torcedor
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
 }
